25dd05c57916ff29f67e148267585022bc5756f9
hasanraza38 <92hasanraza689@gmail.com>
2024-07-28 02:58:43 +0500

    updates

---
+++ config.js
@@ -0,0 +1,17 @@
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+ 
+ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+ const firebaseConfig = {
+   apiKey: "AIzaSyACwlqTQVE5ArKP1_pjrADtW_Ps6RlDsMw",
+   authDomain: "expense-management-app1.firebaseapp.com",
+   projectId: "expense-management-app1",
+   storageBucket: "expense-management-app1.appspot.com",
+   messagingSenderId: "697896152104",
+   appId: "1:697896152104:web:959c67339b61965f54191d",
+   measurementId: "G-1023ZM5Z7R"
+ };
+
+ // Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
